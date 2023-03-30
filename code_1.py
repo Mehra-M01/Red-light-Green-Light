133e3a08f3361a987b8cca7354402d35f0e5d90e
Manan Mehra <101599958+Mehra-M01@users.noreply.github.com>
2023-03-30 23:56:23 +0530

    Add files via upload

--- code_1.py
+++ code_1.py
@@ -1,3 +1,12 @@
+# This project runs in this following fashion,
+#1 - Check if player is in the frame or not
+#2 - Once player in frame then play audio Green light
+#3 - Allow player to move until some random time gap
+#4 - Play red light
+#5 - Now check if player made any movement or not
+#6 - If any movement made then Dead
+#7 - If progress gets more than 100 then Winner.
+
 import mediapipe as mp
 import cv2
 import numpy as np
@@ -39,8 +48,8 @@ mp_pose = mp.solutions.pose  # drawing landmark on screen to show visuals on scr
 pose = mp_pose.Pose()
 drawing = mp.solutions.drawing_utils
 
-image_1=cv2.imread('im1.png') # For green light
-image_2=cv2.imread('im2.png') # For red light
+image_1=cv2.imread('image_1.png') # For green light
+image_2=cv2.imread('image_2.png') # For red light
 
 currWindow = image_1
 
@@ -63,7 +72,7 @@ while True:
 
             if inFrame == 1: # if user in frame
                 if not(isInit):
-                    playsound('greenlight.mp3')
+                    playsound('greenLight.mp3')
                     currWindow = image_1
                     t0= time.time()
                     t1= t0
@@ -94,6 +103,40 @@ while True:
                             cstart = time.time()
                             cEnd = cstart
                             currWindow = image_2
-                            playsound('redlight.mp3')
-                            
+                            playsound('redLight.mp3')
+                            usersum = calc_sum(res.pose_landmarks.landmark)
+                        if (cEnd - cstart) <=3:
+                            tempsum = calc_sum(res.pose_landmarks.landmark)
+                            cEnd = time.time()
+                            if abs(tempsum - usersum) >150:
+                                print("DEAD" , abs(tempsum - usersum))
+                                isAlive = 0 # if true,user is dead
 
+                            else:
+                                isInit = False # so again these values can be updated after one stop
+                                isCinit = False
+                                
+                cv2.circle(currWindow, ((55*6*cpos),200),15, (0,0,255), -1)
+
+                mainWin = np.concatenate(cv2.resize((frm, (800,400)), currWindow), axis = 0)
+                cv2.imshow("MAIN WINDOW", mainWin)
+                    
+            else:
+                cv2.putText(frm, "PEASE MAKE SURE YOU ARE IN FRAME", (20,200), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0), 4)
+                cv2.imshow("WINDOW",frm)
+            if cv2.waitKey(1) == 27 or isAlive == 0 or winner == 1:  # IF any of the following happens program ends
+                cv2.destroyAllWindows()
+                capture.release()
+                break
+
+frm = cv2.blur(frm, (5,5))
+
+if isAlive == 0:
+    cv2.putText(frm, "YOU ARE DEAD", (50,200), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255), 4)
+    cv2.imshow("MAIN WINDOW ", frm)
+
+if winner == 1:
+    cv2.putText(frm, "YOU ARE WINNER", (50,200), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 4)
+    cv2.imshow("MAIN WINDOW ", frm)
+
+cv2.waitKey(0)
